4c5ff33e246e56fdee3667bc3f8fa64cd66b0cb7
Unknown <grizzly.hok@gmail.com>
2018-08-07 20:08:03 +0200

    dictionary working with one option of interaction

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import json
+from difflib import SequenceMatcher
 
 data = json.load(open("data.json"))
 
+word = str.lower(input("Please, introduce the word you want to check: "))
+
+def ment(i):
+	hello = str.lower(input("Did you ment '" + i + "'? For Yes type 'y', for No type 'n'"))
+	if hello == "y":
+		return data[i]
+	else:
+				return "Sorry, we don't find that word in our dictionary."
+
+def meaning(w):
+	if w in data:
+		return data[w]
+	else:
+		for i in data:
+			if SequenceMatcher(None, w, i).ratio() > 0.85:
+				return ment(i)
+		return "Sorry, we don't find that word in our dictionary."
+
+print(meaning(word))
